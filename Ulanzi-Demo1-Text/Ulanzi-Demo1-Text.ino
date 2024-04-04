ce8bf02a7413731a31f9c58781f70f03f5cfb110
Infinite Dreams <8011806+idreamsi@users.noreply.github.com>
2024-04-04 18:43:36 +0330

    Update Ulanzi-Demo1-Text.ino

--- Ulanzi-Demo1-Text/Ulanzi-Demo1-Text.ino
+++ Ulanzi-Demo1-Text/Ulanzi-Demo1-Text.ino
@@ -32,64 +32,63 @@
 // Which pin the LEDs are connected to:
 #define MATRIX_PIN 32
 
-// There is a beeper on pin 15 that is too quiet and is otherwise not very useful.
-#define PIEPSER_PIN 15
+// There is a BUZZER on pin 15 that is too quiet and is otherwise not very useful.
+#define BUZZER_PIN 15
 
 // Number of LEDs in width and height:
-#define MATRIX_BREITE 32
-#define MATRIX_HOEHE 8
+#define MATRIX_WIDTH 32
+#define MATRIX_HEIGHT 8
 
 // The LED matrix as an object:
-CRGB leds[MATRIX_BREITE * MATRIX_HOEHE];
+CRGB leds[MATRIX_WIDTH * MATRIX_HEIGHT];
 FastLED_NeoMatrix *matrix;
 
 
 void setup() {
-  // Mute beeper
-  pinMode(PIEPSER_PIN, OUTPUT);
+  // Mute BUZZER
+  pinMode(BUZZER_PIN, OUTPUT);
   digitalWrite(15, LOW);
   
   // Initialize LED matrix:
-  FastLED.addLeds<NEOPIXEL, MATRIX_PIN>(leds, MATRIX_BREITE * MATRIX_HOEHE);
-  matrix = new FastLED_NeoMatrix(leds, MATRIX_BREITE, MATRIX_HOEHE,
+  FastLED.addLeds<NEOPIXEL, MATRIX_PIN>(leds, MATRIX_WIDTH * MATRIX_HEIGHT);
+  matrix = new FastLED_NeoMatrix(leds, MATRIX_WIDTH, MATRIX_HEIGHT,
      NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG);
   matrix->begin();
 
-  // Brightness goes from 0-255, we take a medium value:
+ // Brightness goes from 0-255, we take a medium value:
   matrix->setBrightness(155);
 
-  // Text-Farbe einstellen: Orange!
-  // Hier kann man Farben auswaehlen und die Rot/Gruen/Blau-Werte ablesen:
+  // Set text color: Orange!
+  // Here you can select colors and read the red/green/blue values:
   // https://www.rapidtables.org/de/web/color/RGB_Color.html
   matrix->setTextColor(matrix->Color(255, 128, 0));
   
-  // Wenn der Text zu lang ist, nicht eine neue Zeile anfangen (die LED-Matrix
-  // kann nur eine Zeile darstellen)
+  // If the text is too long, don't start a new line (the LED matrix
+  // can only display one line)
   matrix->setTextWrap(false);
   
-  // Alles auf der Anzeige loeschen:
+  // Clear everything on display:
   matrix->clear();
 
-  // Start-Position fuer den Text einstellen.
-  // (0, 0) bedeutet (x=0, y=0), was oben links ist. (31, 7) ist die rechte
-  // untere Ecke der Anzeige.
-  // Wichtig: Wenn man eine andere Schriftart einstellt, muss man die Start-Position
-  // auf (0,7) setzen, also unten links:  matrix->setCursor(0, 7);
+  // Set start position for the text.
+  // (0, 0) means (x=0, y=0), which is top left. (31, 7) is the right one
+  // bottom corner of the display.
+  // Important: If you set a different font, you have to change the start position
+  // set to (0,7), i.e. bottom left: matrix->setCursor(0, 7);
   matrix->setCursor(0, 0);
   
-  // Text ausgeben. Es passt nicht alles auf die Anzeige.
+  // Output text. Not everything fits on the display.
   matrix->print("Hallihallo");
 
-  // Und noch eine Linie quer von oben links nach unten rechts darueber zeichnen.
-  // Der Text wurde zuerst gedruckt, die Linie danach. Deshalb ist die Linie ueber
-  // dem Text und verdeckt ihn.
+  // And draw another line across it from top left to bottom right.
+  // The text was printed first, the line after. That's why the line is over
+  // the text and hides it.
   matrix->drawLine(0, 0, 31, 7, matrix->Color(80, 200, 80));
   
-  // Alles anzeigen.  Wichtig: Wenn man das vergisst, wird nichts angezeigt.
+  // Show all. Important: If you forget this, nothing will be displayed.
   matrix->show();
   
 }
 
 void loop() {
-  // Leer, die Anzeige soll ja nicht veraendert werden.
 }

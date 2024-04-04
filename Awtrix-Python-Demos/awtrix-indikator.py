d6b375fd1e40605a47438b2913d4ae93547fa96a
Infinite Dreams <8011806+idreamsi@users.noreply.github.com>
2024-04-04 19:24:12 +0330

    Update awtrix-indikator.py

--- Awtrix-Python-Demos/awtrix-indikator.py
+++ Awtrix-Python-Demos/awtrix-indikator.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python3
 
-# Programm zum Setzen der Indikatoren der AWTRIX-LED-Matrix
-# Beispiel: Setzen des oberen Indikators (Nummer 1) auf rot:
-#           ./awtrix-indikator.py 1 ff0000
-# Beispiel: Ausschalten des oberen Indikators:
-#           ./awtrix-indikator.py 1 aus
-# Siehe https://blueforcer.github.io/awtrix-light/#/api?id=colored-indicators
+# Program for setting the indicators of the AWTrix LED matrix
+# Example: Setting the upper indicator (number 1) on red:
+# ./awtrix-indicator.py 1 ff0000
+# Example: Switching off the upper indicator:
+# ./awtrix-indicator.py 1 off
+# See https://blueforcer.github.io/awtrix-light/#/api?id=colored-indicators
 
 import requests
 import json
 import argparse
 
-# Kommandozeilenargumente einlesen
+# Read command line arguments
 parser = argparse.ArgumentParser(description='AWTRIX Fernsteuerung: Indikatoren')
 
-# Welche Parameter sind moeglich?
+# Which parameters are possible?
 parser.add_argument('indikator', type=int, help='Die Nummer des Indikators (1-3')
 parser.add_argument('farbe', type=str, help='Die Farbe als RGB Hexadezimalwert oder "aus"')
 
 args = parser.parse_args()
 
-# Falls die Farbe "aus" ist, dann wird das durch die Farbe Schwarz (000000) erreicht
+# If the color is "off", this is achieved by the color black (000000)
 if args.farbe == "aus":
     args.farbe = "000000"
 
-# Jetzt haben wir alle Daten fuer den HTTP-Request
+# Now we have all the data for the http request
 url = f"http://10.0.0.73/api/indicator{args.indikator}"
 data = {"color": f"#{args.farbe}"}
-# Dieser Header ist noetig, weil wir JSON-Daten senden
+# This header is set up because we send JSON data
 headers = {'Content-Type': 'application/json'}
 
-# Und: Abschicken!
+# And: Submit!
 response = requests.post(url, data=json.dumps(data), headers=headers)
 
-# Jetzt die Antwort ausgeben
+# Now output the answer
 print(response.status_code)
 print(response.text)
 
